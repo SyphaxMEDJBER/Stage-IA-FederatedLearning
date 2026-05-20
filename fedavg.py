7f99d9ef4759cbb03a34fd1a7a8cab70cf728780
SyphaxMEDJBER <syphax.medjber.8@gmail.com>
2026-05-20 23:24:06 +0200

    code analysé , compris et commenté , 60%

--- fedavg.py
+++ fedavg.py
@@ -10,39 +10,39 @@ Objectif :
 
 from typing import Dict, List, Tuple
 
-import flwr as fl
-import numpy as np
-import tensorflow as tf
+import flwr as fl #gère le federated learning
+import numpy as np # sert à manipuler des tableaux numériques et les poids du modèle
+import tensorflow as tf # sert à créer et entrainer le modèle ia 
 
-from flwr.common import Metrics
-from flwr_datasets import FederatedDataset
-from flwr_datasets.partitioner import DirichletPartitioner
-from flwr.simulation.ray_transport.utils import enable_tf_gpu_growth
+from flwr.common import Metrics # le type Metrics utilisé pour stocker les métriques comme accuracy ou loss 
+from flwr_datasets import FederatedDataset #une classe permettant de changer un dataset déjà existant
+from flwr_datasets.partitioner import DirichletPartitioner #pour repartir les données entre plusieurs clients de maniere aléatoire et unifi
+from flwr.simulation.ray_transport.utils import enable_tf_gpu_growthévite    #les problèmes liés à l’utilisation du GPU avec TensorFlow pendant les simulations Flower.
 
 
 # ============================================================
 # 1. Paramètres globaux
 # ============================================================
 
-VERBOSE = 0
+VERBOSE = 0 #desactive l'affichage détaillé de tenserflow pendant l'entrainement
 
-NUM_CLIENTS = 5
-NUM_ROUNDS = 10
+NUM_CLIENTS = 5  # nombre de client dans la simulation
+NUM_ROUNDS = 10 #noombre de cycle
 
-FRACTION_FIT = 0.1
-FRACTION_EVALUATE = 0.1
+FRACTION_FIT = 0.1 # pourcentage des clients utilisé pour le l'entrainement à chaque round (ex: 10%  des rounds)
+FRACTION_EVALUATE = 0.1 #pourcentage de client utilisé pour l'évaluation de modele 
 
-BATCH_SIZE = 32
-LOCAL_EPOCHS = 5
+BATCH_SIZE = 32 # nombre d'images envoyées au modele a chaque etape d'entrainement (ici le modele traite 32 images a la fois )
+LOCAL_EPOCHS = 5 #nombre de fois ou chaque client entraine localement ses données
 
-TOTAL_DATASET_SIZE = 60000
+TOTAL_DATASET_SIZE = 60000 # taille de dataset utilisé pour la simulation
 
 
 # ============================================================
 # 2. Définition du modèle local
 # ============================================================
 
-def get_model() -> tf.keras.Model:
+def get_model() -> tf.keras.Model: # fonction pour créer le modele
     """
     Crée un modèle simple pour MNIST.
 
@@ -50,21 +50,22 @@ def get_model() -> tf.keras.Model:
     Le modèle prend une image en entrée et prédit la classe correspondante.
     """
 
-    model = tf.keras.Sequential([
-        tf.keras.layers.Input(shape=(28, 28)),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10, activation="softmax"),
+    model = tf.keras.Sequential([  #création du modèle composé de plusieurs couches
+        tf.keras.layers.Input(shape=(28, 28)),# le model recoit une image MNIST de taile 28*28 pixels
+        tf.keras.layers.Flatten(),#transforme l'image en un seul ligne de nombre 28*28=784 valuurs
+        tf.keras.layers.Dense(128, activation="relu"),#couche de 128 neurones qui apprend les caractéristiques 
+        #importantes des images,fonction dactivation 
+        tf.keras.layers.Dropout(0.2),#désactive 20% des neurones pendant l'entrainement pour eviter d'apprendre par coeur
+        tf.keras.layers.Dense(10, activation="softmax"),#couche finale avec 10 sorties car il y a 10 chiffres possibles
     ])
 
-    model.compile(
-        optimizer="adam",
-        loss="sparse_categorical_crossentropy",
-        metrics=["accuracy"],
+    model.compile(   #prepare le modele pour lentrainement 
+        optimizer="adam",  #algo qui ameliore le modele pendant lapprentissage
+        loss="sparse_categorical_crossentropy", # la fonction qui mesure les erreurs du modèle
+        metrics=["accuracy"],# demande d'afficher la precision de modèle
     )
 
-    return model
+    return model # retourne le modèle pret a etre utilisé par les clients  
 
 
 # ============================================================
@@ -84,18 +85,18 @@ class FlowerClient(fl.client.NumPyClient):
     l'entraîne localement, puis renvoie ses poids mis à jour.
     """
 
-    def __init__(self, trainset, valset, cid: str) -> None:
-        self.cid = cid
-        self.model = get_model()
+    def __init__(self, trainset, valset, cid: str) -> None:# initialisation des champs  du client flower
+        self.cid = cid #id du client 
+        self.model = get_model()# chaque client crée sa propre copie locale du modèle IA
 
-        self.trainset = trainset.to_tf_dataset(
-            columns="image",
-            label_cols="label",
-            batch_size=BATCH_SIZE,
-            shuffle=True,
+        self.trainset = trainset.to_tf_dataset( #ransforme les données d’entraînement en dataset TensorFlow utilisable par le modèle
+            columns="image", #indique que les images sont les données d'entrée
+            label_cols="label", #indique les bonnes réponses attendus
+            batch_size=BATCH_SIZE, # envoie les images par groupes de 32 pendant l'entrainement
+            shuffle=True, # mélange les données aleatoirement avant l'netrainement, ca ameliore souvent l'apprentissage
         )
 
-        self.valset = valset.to_tf_dataset(
+        self.valset = valset.to_tf_dataset(#prépare aussi le dataset de validation TensorFlow
             columns="image",
             label_cols="label",
             batch_size=BATCH_SIZE,
@@ -120,15 +121,15 @@ class FlowerClient(fl.client.NumPyClient):
         4. Il renvoie les nouveaux paramètres au serveur.
         """
 
-        self.model.set_weights(parameters)
+        self.model.set_weights(parameters)#le client recoit les param du modèle global et met à jour les siens
 
-        self.model.fit(
-            self.trainset,
-            epochs=LOCAL_EPOCHS,
-            verbose=VERBOSE,
+        self.model.fit( #lance l'entrainement du modèle ia local du client
+            self.trainset, #utilise la dataset d'entrainement du client
+            epochs=LOCAL_EPOCHS,#nombre de repition de client sur les donnés 
+            verbose=VERBOSE, #controle l'affichage des details tenserFlow , ici = 0 donc détails désactivés
         )
 
-        return self.model.get_weights(), len(self.trainset), {}
+        return self.model.get_weights(), len(self.trainset), {}#le client renvoie ls poids et le nombre de données utilisées par le client pour lentrainement 
 
     def evaluate(self, parameters, config):
         """
@@ -138,14 +139,14 @@ class FlowerClient(fl.client.NumPyClient):
         de validation locales, puis renvoie la loss et l'accuracy.
         """
 
-        self.model.set_weights(parameters)
+        self.model.set_weights(parameters) # mettre a jour les parametres de son model local avec celles reçues de serveur  
 
-        loss, accuracy = self.model.evaluate(
+        loss, accuracy = self.model.evaluate(# lancer l'evaluation du modèle 
             self.valset,
             verbose=VERBOSE,
         )
 
-        return loss, len(self.valset), {"accuracy": accuracy}
+        return loss, len(self.valset), {"accuracy": accuracy} #envoie au serveur la loss et le nombre de données utilisées 
 
 
 # ============================================================
@@ -167,18 +168,20 @@ def get_client_fn(dataset: FederatedDataset):
         Chaque client reçoit une partition différente du dataset.
         """
 
-        client_dataset = dataset.load_partition(int(cid), "train")
+        client_dataset = dataset.load_partition(int(cid), "train")#charge la partie du dataset apparetenant à ce client 
 
-        client_dataset_splits = client_dataset.train_test_split(
-            test_size=0.1
+        client_dataset_splits = client_dataset.train_test_split( #sépare les données du client ,entrainement/validation  
+            test_size=0.1 # 10% pour la validation et 90% pour lentrainement 
         )
 
-        trainset = client_dataset_splits["train"]
-        valset = client_dataset_splits["test"]
+        trainset = client_dataset_splits["train"]#données utilisées pour entrainer le modèle
+        valset = client_dataset_splits["test"]#données utilisées pour validation
 
-        return FlowerClient(trainset, valset, cid).to_client()
+        return FlowerClient(trainset, valset, cid).to_client() #creation du client (appel au constructeur)
+        #to_client pour transformer  l'objet client a un format special compatible avec le systeme de 
+        # fLOWER 
 
-    return client_fn
+    return client_fn  # retourne la fonction de creation des clients 
 
 
 # ============================================================
@@ -193,18 +196,18 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     Cela évite de donner le même poids à un petit client et à un grand client.
     """
 
-    accuracies = [
-        num_examples * metric["accuracy"]
-        for num_examples, metric in metrics
+    accuracies = [ 
+        num_examples * metric["accuracy"] # on multiplie chaque  champs metric[accuracy] de metrics par le nombre d'exemple de la meme entrée
+        for num_examples, metric in metrics   
     ]
 
-    examples = [
+    examples = [  # recupère le nombre de données utilisées par chaque clients 
         num_examples
         for num_examples, _ in metrics
     ]
 
     return {
-        "accuracy": sum(accuracies) / sum(examples)
+        "accuracy": sum(accuracies) / sum(examples)  # la moyennen globale 
     }
 
 
@@ -297,7 +300,7 @@ strategy = fl.server.strategy.FedAvg(
 
 client_resources = {
     "num_cpus": 1,
-    "num_gpus": 1,
+    "num_gpus": 1 if tf.config.list_physical_devices("GPU") else 0,
 }
 
 
